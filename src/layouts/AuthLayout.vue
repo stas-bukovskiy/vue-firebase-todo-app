0cb5f6fceeef1286bee5e3ef445152970169a862
stas-bk <s.bukovskyi@aumify.io>
2024-04-23 09:38:21 +0300

    Add AuthLayout

---
+++ src/layouts/AuthLayout.vue
@@ -0,0 +1,20 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+
+    <q-page-container class="flex flex-center">
+      <router-view/>
+    </q-page-container>
+
+  </q-layout>
+</template>
+
+<script>
+export default {
+  setup() {
+  }
+}
+</script>
+
+<style>
+
+</style>
