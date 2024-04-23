1db7df8116822c90a5903dca8e1492e7b74e9c0b
stas-bk <s.bukovskyi@aumify.io>
2024-04-23 11:13:08 +0300

    Add firebase initializing

---
+++ src/boot/firebase.js
@@ -0,0 +1,20 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import { getDatabase } from "firebase/database";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD1VpPytQ4TsYGEhkLLTGE7IRwuMIEFIU0",
+  authDomain: "vue-firebase-todo-app-b1969.firebaseapp.com",
+  databaseURL: "https://vue-firebase-todo-app-b1969-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "vue-firebase-todo-app-b1969",
+  storageBucket: "vue-firebase-todo-app-b1969.appspot.com",
+  messagingSenderId: "949999979903",
+  appId: "1:949999979903:web:f6a249d85f3d40bdf05e4a"
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+const firebaseAuth = getAuth(firebaseApp);
+const database = getDatabase(firebaseApp);
+
+export { firebaseApp, firebaseAuth, database };
